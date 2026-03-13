7a3e94f652e931679674e218bf7a7502bee089e2
Anatoly Frolov (anafro) <anatolyfroloff@gmail.com>
2026-03-13 17:40:55 +1000

    feat(pgsql): add init.sql file as a secret

--- src/main.py
+++ src/main.py
@@ -26,11 +26,13 @@ def main() -> None:
     postgresql_password = generate_secret(alphanumeric, 128)
     postgresql_database = "hyperstream"
     postgresql_kraken_database = "hyperstream_kraken"
+    postgresql_remixer_database = "hyperstream_remixer"
     aws_access_key = "test"
     aws_secret_key = "test"
     aws_region = "us-east-1"
     aws_leonardo_s3_bucket = generate_secret(alpha_lc, 16)
     aws_kraken_s3_bucket = generate_secret(alpha_lc, 16)
+    aws_remixer_s3_bucket = generate_secret(alpha_lc, 16)
     rabbitmq_username = generate_secret(alpha, 16)
     rabbitmq_password = generate_secret(alphanumeric, 128)
     rabbitmq_exchange = "hyperstream.events"
@@ -39,11 +41,13 @@ def main() -> None:
     vault.new_file("postgresql-password.txt", postgresql_password)
     vault.new_file("postgresql-database.txt", postgresql_database)
     vault.new_file("postgresql-kraken-database.txt", postgresql_kraken_database)
+    vault.new_file("postgresql-remixer-database.txt", postgresql_remixer_database)
     vault.new_file("aws-access-key.txt", aws_access_key)
     vault.new_file("aws-secret-key.txt", aws_secret_key)
     vault.new_file("aws-region.txt", aws_region)
     vault.new_file("aws-leonardo-s3-bucket.txt", aws_leonardo_s3_bucket)
     vault.new_file("aws-kraken-s3-bucket.txt", aws_kraken_s3_bucket)
+    vault.new_file("aws-remixer-s3-bucket.txt", aws_remixer_s3_bucket)
     vault.new_file("rabbitmq-username.txt", rabbitmq_username)
     vault.new_file("rabbitmq-password.txt", rabbitmq_password)
     vault.new_file("rabbitmq-exchange.txt", rabbitmq_exchange)
@@ -53,15 +57,6 @@ def main() -> None:
         {
             "rabbitmq_username": rabbitmq_username,
             "rabbitmq_password": rabbitmq_password,
-        },
-    )
-
-    vault.new_file_from_template(
-        "postgres.sql",
-        {
-            "postgresql_username": postgresql_username,
-            "postgresql_password": postgresql_password,
-            "postgresql_database": postgresql_database,
         },
     )
 
@@ -84,6 +79,18 @@ def main() -> None:
     POSTGRESQL_DATABASE={postgresql_database}
     """),
     )
+
+    vault.new_file(
+        "init.sql",
+        "\n".join(
+            f"CREATE DATABASE {db};"
+            for db in [
+                postgresql_kraken_database,
+                postgresql_remixer_database,
+            ]
+        ),
+    )
+
     logger.info("Secrets created")
 
     logger.info("Downloading dependencies...")
--- templates/postgres.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER `$postgresql_user` WITH PASSWORD `$postgresql_password`;
-CREATE DATABASE IF NOT EXISTS `$postgresql_database`;
-GRANT ALL PRIVILEGES ON `$postgresql_database` TO `$postgresql_user`;
